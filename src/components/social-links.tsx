f8d5841582acc9e5c2638250efaaa362e2f96eba
Jannis Isensee <jannis.isensee@gmail.com>
2022-10-16 18:34:17 +0200

    improve social link display

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -8,8 +8,18 @@ import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Socials } from '@prisma/client'
 import { FC, ReactNode } from 'react'
-import { Button } from 'react-daisyui'
+import { Button, Tooltip } from 'react-daisyui'
 import { Link } from './link'
+import { useCopyButton } from '@/hooks/misc-hooks'
+
+const isUrl = (str: string) => {
+  try {
+    const url = new URL(str)
+    return ['http:', 'https:'].includes(url.protocol)
+  } catch {
+    return false
+  }
+}
 
 type SocialLinksProps = {
   socials: Socials
@@ -19,12 +29,13 @@ type SocialLinksProps = {
 
 type SocialLinkProps = {
   icon: IconDefinition
-  link?: string | null
+  link: string
   compact?: boolean
   text: ReactNode
 }
-const SocialLink: FC<SocialLinkProps> = ({ icon, link, compact, text }) =>
-  link ? (
+const SocialLink: FC<SocialLinkProps> = ({ icon, link, compact, text }) => {
+  const { onCopy, buttonText } = useCopyButton(link, 'Copied!')
+  return isUrl(link) ? (
     <>
       {compact ? (
         <Link href={link} target='_blank'>
@@ -41,7 +52,24 @@ const SocialLink: FC<SocialLinkProps> = ({ icon, link, compact, text }) =>
         </Link>
       )}
     </>
-  ) : null
+  ) : (
+    <Tooltip message='Click to copy'>
+      <Button
+        className='hover:bg-primary hover:text-primary-content'
+        startIcon={<FontAwesomeIcon icon={icon} size='2x' />}
+        onClick={() => onCopy(link)}
+      >
+        {buttonText}
+      </Button>
+    </Tooltip>
+  )
+}
+
+const correctHandle = (handle: string) =>
+  handle.startsWith('@') ? handle.substring(1) : handle
+
+const correctLink = (link: string, prefix: string) =>
+  isUrl(link) ? link : prefix + correctHandle(link)
 
 export const SocialLinks: FC<SocialLinksProps> = ({
   socials,
@@ -49,29 +77,37 @@ export const SocialLinks: FC<SocialLinksProps> = ({
   compact,
 }) => (
   <div className={className}>
-    <SocialLink
-      link={socials.website}
-      icon={faGlobe}
-      text='Website'
-      compact={compact}
-    />
-    <SocialLink
-      link={socials.twitter}
-      icon={faTwitter}
-      text='Twitter'
-      compact={compact}
-    />
-    <SocialLink
-      link={socials.discord}
-      icon={faDiscord}
-      text='Discord'
-      compact={compact}
-    />
-    <SocialLink
-      link={socials.github}
-      icon={faGithub}
-      text='Github'
-      compact={compact}
-    />
+    {socials.website && (
+      <SocialLink
+        link={socials.website}
+        icon={faGlobe}
+        text='Website'
+        compact={compact}
+      />
+    )}
+    {socials.twitter && (
+      <SocialLink
+        link={correctLink(socials.twitter, 'https://twitter.com/')}
+        icon={faTwitter}
+        text='Twitter'
+        compact={compact}
+      />
+    )}
+    {socials.discord && (
+      <SocialLink
+        link={socials.discord}
+        icon={faDiscord}
+        text='Discord'
+        compact={compact}
+      />
+    )}
+    {socials.github && (
+      <SocialLink
+        link={correctLink(socials.github, 'https://github.com/')}
+        icon={faGithub}
+        text='Github'
+        compact={compact}
+      />
+    )}
   </div>
 )
